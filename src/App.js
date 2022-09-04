ea9e663393b0bc9df89963cccbe8a727bf441f53
lukrycyfa <64338703+lukrycyfa@users.noreply.github.com>
2022-09-05 00:01:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -291,11 +291,9 @@ function App() {
           <>
     
             <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
-           {
-                nftMetaData.map((nftMetaData) => {
-                  return (
+
                     <div className="card bg-base-100 shadow-xl col-span-1" key={nftMetaData.edition}>
-                    {console.log(nftMetaData)}
+
                       <figure><img src={nftMetaData.image} alt={nftMetaData.name} /></figure>
                       <div className="card-body">
                         <h2 className="card-title text-white">{nftMetaData.name}</h2>
@@ -307,9 +305,6 @@ function App() {
                         </div>
                       </div>
                     </div>
-                  );
-                })
-              }
             </div>
             <p className="mt-5 text-center text-white font-bold">{mintStatus}</p>
             <p className="mt-5 text-center text-amber-400	"><span className="font-bold text-white">Your Wallet Address: </span>{yourWalletAddress}</p>
